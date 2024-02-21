9298a8382eb338b909db90f76a4662c1c235aa0b
vagosu <victorss_26@hotmail.com>
2024-02-21 18:54:49 +0100

    Ejercicio 5-6

--- src/main/java/ejercicio5y6/Main.java
+++ src/main/java/ejercicio5y6/Main.java
@@ -2,8 +2,10 @@ package ejercicio5y6;
 
 public class Main {
     public static void main (String[] args){
-
-
+        Bienes [] lista = new Bienes[4];
+        lista[0] = new Juguete("Miau", 15, 5);
+        lista[1] = new Comida("macarrone", 1, 60);
+        lista[2] = new Libro("mondongo", 2, "mmmm" );
 
 
 
